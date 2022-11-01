ca8ac63e8f76a2d32225ea3dd4070c5556284922
Denni Knudsen <37368491+Deknudsen@users.noreply.github.com>
2022-11-01 01:20:54 +0100

    EBB5-5815

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from 'firebase/auth';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBsiz45ybZagYvGG0gkEDzocU12tIMuyzg",
+  authDomain: "advancedjsexamsproject.firebaseapp.com",
+  projectId: "advancedjsexamsproject",
+  storageBucket: "advancedjsexamsproject.appspot.com",
+  messagingSenderId: "707458761356",
+  appId: "1:707458761356:web:b67698e3c2ede07878f1a9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+
+export const auth = getAuth(app);
